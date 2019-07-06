6c86323a34aaa30deb905bc4be18c0351b25d6c8
Siarhei Straltsou <straltsou.siarhei@gmail.com>
2019-07-06 17:42:06 +0300

    eliminate chrom driver path

--- conftest.py
+++ conftest.py
@@ -13,8 +13,8 @@ def browser(request):
     language = request.config.getoption('language')
     chrome_options = Options()
     chrome_options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    chrome_d_path = '/opt/Chrome/chromedriver'
-    browser = webdriver.Chrome(options=chrome_options, executable_path=chrome_d_path)
+    # chrome_d_path = '/opt/Chrome/chromedriver'
+    browser = webdriver.Chrome(options=chrome_options)  #, executable_path=chrome_d_path)
     print('Language is: {}'.format(language))
     yield browser
     print('\nquit browser...')
